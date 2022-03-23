a53fa3f871b78c92e92601a8e6bcf2534b14e22e
diego50to <diegofernandosoto@gmail.com>
2022-03-23 13:38:39 -0300

    Actualizacion WebApi Backend

--- asp/WebApplication1/WebApplication1/Controllers/PersonaController.cs
+++ asp/WebApplication1/WebApplication1/Controllers/PersonaController.cs
@@ -4,39 +4,61 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class PersonaController : ApiController
     {
         // GET api/<controller>
         public IEnumerable<Persona> Get()
         {
-            return null;
+            GestorPersona gestor = new GestorPersona();
+            return gestor.ListarPersonas();
         }
 
         // GET api/<controller>/5
-        public Saldo Get(int idCuenta )
+        public Persona Get(string mail, string pass)
         {
-            GestorCompra gestorCompra = new GestorCompra();
-
-            return null;
+            GestorPersona gestor = new GestorPersona();
+            return gestor.ListarPersonasPorId(mail, pass);
         }
+
 
         // POST api/<controller>
         public void Post([FromBody] Persona value)
         {
+            GestorPersona gestor = new GestorPersona();
+            gestor.CrearPersona(value);
         }
 
         // PUT api/<controller>/5
-        public void Put(int id, [FromBody] Persona value)
+        public void Put(long Id, [FromBody] Persona persona)
         {
+            GestorPersona gestor = new GestorPersona();
+            gestor.EditarPersona(persona);
+
+            /*Persona usuario = new Persona();
+            if (usuario.Id == id)
+            {
+                GestorPersona gestor = new GestorPersona();
+                gestor.EditarPersona(persona);
+            } else {
+                Ok(new { message = "no encontrado" });
+            }*/
+
+
+            //gestor.EditarPersona(value);
         }
 
         // DELETE api/<controller>/5
         public void Delete(int id)
         {
+            GestorPersona gestor = new GestorPersona();
+            //gestor.EliminarPersona();
         }
     }
+
 }
--- asp/WebApplication1/WebApplication1/Models/Persona.cs
+++ asp/WebApplication1/WebApplication1/Models/Persona.cs
@@ -6,36 +6,80 @@ using System.Web;
 namespace WebApplication1.Models
 {
     public class Persona
-    {  // atributos (campo de bd)
-        int id;
-        string nombre;
-        string apellido;
-        int dni;
-        DateTime fechaNacimiento;
+    {
+        //atributos (Caraterísticas)
+        private long id;
+        private string cuil;
+        private string nombre;
+        private DateTime fnac;
+        private string domicilio;
+        //private string domicilio2;
+        private int idProvincia;
+        private string pais;
+        private string tipo;
+        private string estado;
+        private DateTime fechaAlta;
+        private string cbu;
+        private string banco;
+        private string dniFrente;
+        private string dniDorso;
+        private string pass;
+        private string mail;
+        private string telefono;
 
-        // propiedades getter y setter 
-        public int Id { get => id; set => id = value; }
+        //Propiedades Getters y Setters
+        public long Id { get => id; set => id = value; }
+        public string Cuil { get => cuil; set => cuil = value; }
         public string Nombre { get => nombre; set => nombre = value; }
-        public string Apellido { get => apellido; set => apellido = value; }
-        public int Dni { get => dni; set => dni = value; }
-        public DateTime FechaNacimiento { get => fechaNacimiento; set => fechaNacimiento = value; }
+        public DateTime Fnac { get => fnac; set => fnac = value; }
+        public string Domicilio { get => domicilio; set => domicilio = value; }
+        public int IdProvincia { get => idProvincia; set => idProvincia = value; }
+        public string Pais { get => pais; set => pais = value; }
+        public string Tipo { get => tipo; set => tipo = value; }
+        public string Estado { get => estado; set => estado = value; }
+        public DateTime FechaAlta { get => fechaAlta; set => fechaAlta = value; }
+        public string Cbu { get => cbu; set => cbu = value; }
+        public string Banco { get => banco; set => banco = value; }
+        public string DniFrente { get => dniFrente; set => dniFrente = value; }
+        public string DniDorso { get => dniDorso; set => dniDorso = value; }
+        public string Pass { get => pass; set => pass = value; }
+        public string Mail { get => mail; set => mail = value; }
+        public string Telefono { get => telefono; set => telefono = value; }
 
-        // contructores
 
-        public Persona(int id, string nombre, string apellido, int dni, DateTime fechaNacimiento)
+        // Constructores
+        public Persona(long id, string nombre, string cuil, DateTime fnac, string domicilio, int idProvincia
+        , string pais, string tipo, string estado, DateTime fechaAlta, string cbu, string banco, string dniFrente
+        , string dniDorso, string pass, string mail, string telefono)
         {
             this.id = id;
+            this.cuil = cuil;
             this.nombre = nombre;
-            this.apellido = apellido;
-            this.dni = dni;
-            this.fechaNacimiento = fechaNacimiento;
+            this.fnac = fnac;
+            this.domicilio = domicilio;
+            this.idProvincia = idProvincia;
+            this.pais = pais;
+            this.tipo = tipo;
+            this.estado = estado;
+            this.fechaAlta = fechaAlta;
+            this.cbu = cbu;
+            this.banco = banco;
+            this.dniFrente = dniFrente;
+            this.dniDorso = dniDorso;
+            this.pass = pass;
+            this.mail = mail;
+            this.telefono = telefono; ;
         }
 
-        // metodos (comportamientos)
-         public Persona()
+        // Métodos
+        public Persona()
         {
 
         }
-        
+
+        public void saludar()
+        {
+
+        }
     }
 }
